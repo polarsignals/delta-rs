b8ce17657449fa198dafe8ee7b173f2f5b183089
Alfonso Subiotto Marques <alfonso.subiotto@polarsignals.com>
2025-05-28 14:21:45 +0200

    transaction/state: make public

--- crates/core/src/kernel/transaction/mod.rs
+++ crates/core/src/kernel/transaction/mod.rs
@@ -109,7 +109,7 @@ pub(crate) mod application;
 mod conflict_checker;
 mod protocol;
 #[cfg(feature = "datafusion")]
-pub(crate) mod state;
+pub mod state;
 
 const DELTA_LOG_FOLDER: &str = "_delta_log";
 pub(crate) const DEFAULT_RETRIES: usize = 15;
